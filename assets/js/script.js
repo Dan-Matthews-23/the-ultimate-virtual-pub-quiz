153b9d7cad3c472095e8bfe7347c734c7b9d6131
Dan Matthews <dan.matthews1987@gmail.com>
2023-08-19 12:59:36 +0100

    UPDATED scrip-t.js to correct an error with Return Home button on GitHub

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,7 @@ addEventListener('click', function (event)
 
   } else if (event.target === returnHome) 
   {
-      window.location.assign("../../index.html");
+      window.location.assign("https://dan-matthews-23.github.io/the-ultimate-virtual-pub-quiz/index.html");
   } else if  (event.target === viewHighScoresPostGame) 
   {
     
