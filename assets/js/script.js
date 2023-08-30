80fdb1bfd084e5fd25741e293edd3d6e5facc626
Dan Matthews <dan.matthews1987@gmail.com>
2023-08-30 17:27:35 +0100

    UPDATED script, style with new formatting, UPDATED readme with new content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
-//Global Variables
-const nextButton = document.getElementById("next");
-nextButton.addEventListener("click", retriveNextQuestion);
-
 // URL for API
-const difficultyEasy = "https://opentdb.com/api.php?amount=20&type=multiple";
+const apiURL = "https://opentdb.com/api.php?amount=20&type=multiple";
 
+//---Set default variable values---//
 let connect = {};
 let score = 0;
-
 let id = 0;
 let qID = 1;
 const scoreCounter = document.getElementById("score");
 const questionCounter = document.getElementById("question-number");
+//---End of default variables---//
 
+//---Set the values of each button preperation for the Event Listeners---//
 const showQuiz = document.getElementById("showQuizBtn");
 const showInst = document.getElementById("showInstBtn");
 const showHall = document.getElementById("showHallBtn");
@@ -20,26 +18,26 @@ const returnHome = document.getElementById("return-btn");
 const enterUsernameBtn = document.getElementById("enter-username");
 const viewHighScoresPostGame = document.getElementById("post-game-scores");
 const reset = document.getElementById("reset");
+const nextButton = document.getElementById("next");
+nextButton.addEventListener("click", retriveNextQuestion);
 
+//---Event Listeners---//
 addEventListener('click', function (event) {
    if (event.target === showQuiz) {
       document.getElementById("return-section").classList.remove("hidden");
       document.getElementById("user-selection-section").classList.add("hidden");
       document.getElementById("username-section").classList.remove("hidden");
-
    } else if (event.target === enterUsernameBtn) {
       checkRoomCode();
    } else if (event.target === showInst) {
       document.getElementById("return-section").classList.remove("hidden");
       document.getElementById("user-selection-section").classList.add("hidden");
       document.getElementById("instructions-section").classList.remove("hidden");
-
    } else if (event.target === showHall) {
       document.getElementById("return-section").classList.remove("hidden");
       document.getElementById("user-selection-section").classList.add("hidden");
       document.getElementById("hall-of-fame-section").classList.remove("hidden");
       displayHighScore();
-
    } else if (event.target === reset) {
       window.location.assign("https://dan-matthews-23.github.io/the-ultimate-virtual-pub-quiz/index.html"); // WHEN DEPLOYED
    } else if (event.target === returnHome) {
@@ -51,31 +49,27 @@ addEventListener('click', function (event) {
       document.getElementById("hall-of-fame-section").classList.remove("hidden");
       document.getElementById("game-over-modal").classList.add("hidden");
       displayHighScore();
-
    }
-
 });
+//---End of Event Listeners---//
 
-function checkRoomCode() {
-   const roomCode = document.getElementById("room-code-value").value;
-   if (roomCode < 1 || roomCode === "" || isNaN(roomCode)) {
-      alert("Please enter a valid room code");
-   } else {
-      checkUsername();
+// ---Function to connect to API --- //
+//---This section of code was based on a template from Chris Minnick, from JavaScript ALL-IN-ONE for dummies. See README for more information---//
+async function connectAPI(apiURL) {
+   const connection = await fetch(apiURL);
+   if (connection.status >= 200 && connection.status <= 299) {
+      connect = await connection.json();
+      getQuestionArray();
+         } else {
+      window.location.assign("500.html");
    }
 }
+//----End of code snippet-----//
+//----End of connect to API --- //
 
-function confirmReturn() {
-
-   const ConfRet = confirm("Are you sure you want to return? You will lose all progress");
-   if (ConfRet == true) {
-      window.location.assign("https://dan-matthews-23.github.io/the-ultimate-virtual-pub-quiz/index.html");
-   }
-}
-
+// ---Data validation for Username--- //
 function checkUsername() {
    const username = (document.getElementById("usernameValue").value);
-
    if (username.length === 0) {
       alert("Please enter a username");
    } else if (username.length > 0 && username.length < 3) {
@@ -88,52 +82,74 @@ function checkUsername() {
       document.getElementById("user-selection-section").classList.add("hidden");
       document.getElementById("username-section").classList.add("hidden");
    }
-
 }
+//----End of username validation --- //
 
-// Function to connect to API. 
-//---This section of code was based on a template from Chris Minnick, from JavaScript ALL-IN-ONE for dummies. See README for more information---//
-async function connectAPI(apiURL) {
-   const connection = await fetch(apiURL);
-   if (connection.status >= 200 && connection.status <= 299) {
-      connect = await connection.json();
-      getQuestionArray();
-      //getAnswerArray();
+// ---Data validation for Room Code--- //
+function checkRoomCode() {
+   const roomCode = document.getElementById("room-code-value").value;
+   if (roomCode < 1 || roomCode === "" || isNaN(roomCode)) {
+      alert("Please enter a valid room code");
    } else {
-      window.location.assign("500.html");
+      checkUsername();
    }
 }
-//----END OF CODE SNIPPET-----//
+//----End of Room Code validation --- //
 
-// Display the question in index.html in the part with an ID of 'question'. Do this based on where we are in the array (question number)
+// ---Alert Box on clicking Return to Main Menu--- //
+function confirmReturn() {
+   const ConfRet = confirm("Are you sure you want to return? You will lose all progress");
+   if (ConfRet == true) {
+      window.location.assign("https://dan-matthews-23.github.io/the-ultimate-virtual-pub-quiz/index.html");
+   }
+}
+// ---End of Alert Box function --- //
+
+//---Set the default elements which will then be replaced by variables---//
 function getQuestionArray() {
    const question = (document.getElementById("question").innerHTML = connect.results[id].question);
    const displayScore = (document.getElementById("score").innerHTML = `Score: ${score}`);
    const setRawQuestionNumber = (document.getElementById("question-number").innerHTML = `Q: ${qID}`);
    getAnswerArray(question, displayScore, setRawQuestionNumber);
 }
+//---End of default setting---//
 
-// Get the list of answers to the question we just called. 
+//---Function to call the next question, or end the game if reached the question limit---//
+function retriveNextQuestion() {
+   id++;
+   if (id >= connect.results.length) {
+      document.getElementById("game-over-modal").classList.remove("hidden");
+      document.getElementById("return-section").classList.add("hidden");
+      const finalScore = document.getElementById('final-score');
+      finalScore.innerText = (`<h2>Final score: ${score}</h2>`);
+      document.getElementById("quiz-section").classList.add("hidden");
+      return;
+   }
+   increaseQuestionNumber();
+   getQuestionArray();
+   document.getElementById("next").classList.add("hidden");
+   getAnswerArray();
+}
+//---End of calling next question function---//
+
+//---Function to get the array of answers---//
 function getAnswerArray() {
    const possibleAnswers = document.querySelectorAll(".answer");
    const answerCorrect = connect.results[id].correct_answer;
    const answerIncorrect = connect.results[id].incorrect_answers;
    const newAnswerDataset = answerIncorrect.concat(answerCorrect);
-
    shuffle(newAnswerDataset);
-
    for (let i = 0; i < possibleAnswers.length; i++) {
       possibleAnswers[i].innerHTML = newAnswerDataset[i];
       possibleAnswers[i].style.background = "";
-
       possibleAnswers[i].disabled = false;
       possibleAnswers[i].removeEventListener("click", confirmAnswer);
       possibleAnswers[i].addEventListener("click", confirmAnswer);
    }
-
 }
+//---End of function to get answer array---//
 
-// Function to shuffle all answers in the array
+//---Function to prevent correct answer appearing in same point in array - shuffle answer array---//
 //------This section of code was taken from Free Code Camp and slightly modified to suit the purposes of this function----//
 function shuffle(array) {
    for (let i = array.length - 1; i > 0; i--) {
@@ -141,14 +157,13 @@ function shuffle(array) {
       [array[i], array[j]] = [array[j], array[i]];
    }
 }
-//---END OF CODE SNIPPET------//
+//---End of code snippet------//
+//---End of shuffle answers array---//
 
+//---Function to check the chosen answer matches the correct answer in the API---//
 function confirmAnswer() {
-
-
    const rawselectedAnswer = this.innerHTML;
    const rawCorrectAnswer = connect.results[id].correct_answer;
-
 
    //----This snippet of code was taken from Go Make Things (https://gomakethings.com/decoding-html-entities-with-vanilla-javascript/)----//
    const decodeHTMLCorrect = function (html) {
@@ -161,11 +176,9 @@ function confirmAnswer() {
       txtSelectedAnswer.innerHTML = html;
       return txtSelectedAnswer.value;
    };
-
    const answerCorrect = decodeHTMLCorrect(`${rawCorrectAnswer}`);
    const selectedAnswer = decodeHTMLSelected(`${rawselectedAnswer}`);
-   //----End of code snippet----
-
+   //----End of code snippet----//
 
    if (selectedAnswer === answerCorrect) {
       this.style.background = "green";
@@ -175,56 +188,43 @@ function confirmAnswer() {
       for (let a = 0; a < possibleAnswers.length; a++) {
          possibleAnswers[a].disabled = true;
       }
-
       increaseScore();
-     
-
-   } else {
-    
-
-      this.style.background = "red";
+      } else {
+        this.style.background = "red";
       console.log(`The correct answer is ${answerCorrect}, but you selected ${selectedAnswer}`);
-
       // Display the background color of the correct answer when user selects wrong answer
       const correctAnswerButtons = document.querySelectorAll('.answer');
       for (let a = 0; a < correctAnswerButtons.length; a++) {
          if (correctAnswerButtons[a].innerHTML === answerCorrect) {
             correctAnswerButtons[a].style.background = 'orange';
             document.getElementById("next").classList.remove("hidden");
-
             break;
          }
       }
       const answerButtons = document.querySelectorAll('.answer');
       for (let a = 0; a < answerButtons.length; a++) {
          answerButtons[a].disabled = true;
-
       }
    }
 }
+//---End of check answer function---//
 
-
+//---Function to increase score if answer correct---//
 function increaseScore() {
-
    score += 10;
    scoreCounter.innerText = ` Score: ${score}`;
-  
-   
-   
 }
+//---End of increase score function---//
 
+//---Function to increase question number---//
 function increaseQuestionNumber() {
-
-  
    qID += 1;
    questionCounter.innerText = ` Q) ${qID}`;
-  
-  
-  
 }
+///--End of increase question number function---//
 
+//---Function to add score to Hall of Fame---//
 function addHighScore() {
-
    const existingScores = JSON.parse(localStorage.getItem('existingScores')) || [];
    const roomCode = (document.getElementById("room-code-value").value);
    const username = (document.getElementById("usernameValue").value);
@@ -237,51 +237,22 @@ function addHighScore() {
    };
    existingScores.push(setHighScoreArray);
    existingScores.sort((a, b) => b.playerHighScore - a.playerHighScore);
-
-
    localStorage.setItem('existingScores', JSON.stringify(existingScores));
-
 }
+//---End of function to add to hall of fame---//
 
+//---Function to display high scores from hall of fame---//
 function displayHighScore() {
    const pullExistingScores = JSON.parse(localStorage.getItem('existingScores')) || [];
    const pullInDocumentHighScores = document.getElementById('high-score-table');
-
-
    pullExistingScores.sort((a, b) => (b.playerRoom - a.playerRoom || b.playerHighScore - a.playerHighScore));
-
-
    pullInDocumentHighScores.innerHTML = pullExistingScores.map(entry => `
       <div class="col high-scores-list">${entry.playerName}</div>
       <div class="col high-scores-list">${entry.playerHighScore}</div>
       <div class="col high-scores-list">${entry.playerRoom}</div>`).join('');
-
 }
+//---End of function to dislay Hall of Fame---//
 
-function retriveNextQuestion() {
-   id++;
-
-   if (id >= connect.results.length) {
-      document.getElementById("game-over-modal").classList.remove("hidden");
-      document.getElementById("return-section").classList.add("hidden");
-      const finalScore = document.getElementById('final-score');
-      finalScore.innerText = (`<h2>Final score: ${score}</h2>`);
-
-
-      document.getElementById("quiz-section").classList.add("hidden");
-
-
-      return;
-   }
-   increaseQuestionNumber();
-   getQuestionArray();
-   
-   
-   
-   document.getElementById("next").classList.add("hidden");
-   getAnswerArray();
-
-}
-
-
-connectAPI(difficultyEasy);
+//---Call the API function---//
+connectAPI(apiURL);
+//--------//
